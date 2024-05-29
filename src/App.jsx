75a7c71383df2c01423b7e03b9161959f04777b7
Ahmad Bondan Gunawan <bondangunawan77@gmail.com>
2024-05-30 00:44:03 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
 import Todos from "./components/Todos";
 
 function App() {
@@ -37,10 +38,25 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  const addTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-
+      <TodoForm addTodo={addTodo} />
       <Todos
         todos={todos}
         toggleCompleted={toggleCompleted}
